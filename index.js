de29d8385e2014adf2dc1671f6a81591b8272eb2
akhila paidi <paidiakhila1999@gmail.com>
2023-05-07 02:17:21 +0000

    superOverLeagueApp

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div class="bg-container" id="container">
+    <h1 class="heading">Super Over League</h1>
+    <div class="">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="images"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="images"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
